9dd15469a7bcaf7b1e358f01ac1ca3a22df3ea5c
ATTHIGANGAMOUNIKA <84660729+ATTHIGANGAMOUNIKA@users.noreply.github.com>
2022-12-29 15:39:25 +0530

    #include<stdio.h> int main() {     int n,sum=0,r,temp;     scanf("%d",&n);     temp=n;     while(n>0){         r=n%10;         sum=(10*sum)+r;         n=n/10;     }
        printf("%d",sum);
        
    }

---
+++ reverse_of_a_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,r,temp;
+    scanf("%d",&n);
+    temp=n;
+    while(n>0){
+        r=n%10;
+        sum=(10*sum)+r;
+        n=n/10;
+    }
+
+    printf("%d",sum);
+    
+}
