e3426ba4dee98b036553e05559a69ef4040890b9
ATTHIGANGAMOUNIKA <84660729+ATTHIGANGAMOUNIKA@users.noreply.github.com>
2023-01-02 20:02:36 +0530

    #include<stdio.h> int main(){     int n,l=0,rem;     scanf("%d",&n);     while(n>0)     {          rem=n%10;          if(rem>l){              l=rem;          }          n=n/10;     }     printf("%d",l);     return 0; }

---
+++ Largest_Digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,l=0,rem;
+    scanf("%d",&n);
+    while(n>0)
+    {
+         rem=n%10;
+         if(rem>l){
+             l=rem;
+         }
+         n=n/10;
+    }
+    printf("%d",l);
+    return 0;
+}
